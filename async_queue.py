69009f6d7f21c027720773f04f795438c8fbb9eb
Hynchus <frasem@hotmail.ca>
2018-11-15 15:35:22 -0800

    Fixed hang introduced by previous commit

--- async_queue.py
+++ async_queue.py
@@ -4,6 +4,7 @@ import cerebrate_config as cc
 _loop = None
 
 _queue = []
+_coroutine_queued = asyncio.Event()
 
 
 def initialize_async_queue(event_loop=None):
@@ -21,10 +22,13 @@ def initialize_async_queue(event_loop=None):
 async def _handle_queue():
     global _queue
     while not cc.my_state_event[cc.State.TERMINATING].is_set():
+        await _coroutine_queued.is_set()
+        _coroutine_queued.clear()
         temp_queue = _queue
         for coroutine in temp_queue:
             _queue.remove(coroutine)
             asyncio.ensure_future(coroutine, loop=_loop)
+        
 
 def queue_coroutine(coroutine):
     '''Enqueues the given coroutine to be run in the current event_loop.
@@ -34,3 +38,4 @@ def queue_coroutine(coroutine):
     if not coroutine:
         return
     _queue.append(coroutine)
+    _coroutine_queued.set()
